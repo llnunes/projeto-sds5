5d78fcda2a0b67186bafcf12fef8bef682831abf
Lucas Lopes Nunes <lucas.nunes@qintess.com>
2021-11-04 23:03:34 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import Datatable from "components/Datatable";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Ola mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Ola mundo</h1>
+
+        <Datatable />
+      </div>   
+
+      <Footer />
+    </>
   );
 }
 
