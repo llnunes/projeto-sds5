f28bc05d2f34a0595212086c01e4c97fcf4bd874
Lucas Lopes Nunes <lucas.nunes@qintess.com>
2021-11-04 23:23:24 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import Barchart from "components/Barchart";
 import Datatable from "components/Datatable";
+import Donutchart from "components/Donutchart";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
 
@@ -7,7 +9,22 @@ function App() {
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-primary">Ola mundo</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso</h5> 
+            <Barchart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Total de vendas</h5> 
+            <Donutchart />
+          </div>
+
+        </div>
+        <div className="row px-3">
+          <h2 className="text-primary">Todas vendas</h2> 
+        </div>
 
         <Datatable />
       </div>   
