0d574d99e7489dfe3fd04cd04f943c3118a50b71
Lucas Lopes Nunes <lucas.nunes@qintess.com>
2021-11-06 13:25:04 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dtos/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dtos/SaleDTO.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsvendas.dtos;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.devsuperior.dsvendas.entities.Sale;
@@ -10,13 +11,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
-public class SaleDTO {
+public class SaleDTO implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
+	
+	private SellerDTO seller;
 	
 	public SaleDTO(Sale entity) {
 		this.id = entity.getId();
@@ -24,5 +29,6 @@ public class SaleDTO {
 		this.deals = entity.getDeals();
 		this.amount = entity.getAmount();
 		this.date = entity.getDate();
+		this.seller = new SellerDTO(entity.getSeller());
 	}
 }
